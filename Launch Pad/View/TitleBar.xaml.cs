87d6af8b725746427d7bb6aa39ee1bd3d8b1c2f0
Attila Baradics <baradics.attila@gmail.com>
2026-04-17 15:39:24 +0200

    Hotfix

--- Launch Pad/View/TitleBar.xaml.cs
+++ Launch Pad/View/TitleBar.xaml.cs
@@ -1,44 +1,50 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Launch_Pad.View
 {
-    /// <summary>
-    /// Interaction logic for TitleBar.xaml
-    /// </summary>
-    /// 🗖
     public partial class TitleBar : UserControl
 	{
 		public TitleBar()
 		{
 			InitializeComponent();
 		}
-
-
+		private Window mainwindow(){
+			return Window.GetWindow(this);
+		}
+		
 		private void clsbtn_Click(object sender, RoutedEventArgs e)
 		{
 			Environment.Exit(0);
 		}
 
-        private void maximbtn_Click(object sender, RoutedEventArgs e)
-        {
-            var mainwindow = Window.GetWindow(this);
-            if (mainwindow.WindowState == WindowState.Maximized)
-            {
-                mainwindow.WindowState = WindowState.Normal;
-            }
-            else
-            {
-                mainwindow.WindowState = WindowState.Maximized;
 
-            }
+		private void maximbtn_Click(object sender, RoutedEventArgs e)
+        {
+			if (mainwindow().WindowState == WindowState.Normal)
+			{
+				maximbtn.Content = "";
+				mainwindow().WindowState = WindowState.Maximized;
+
+			}
+			else
+			{
+				maximbtn.Content = "";
+				mainwindow().WindowState = WindowState.Normal;
+
+			}
+		}
+
+		private void minimbtn_Click(object sender, RoutedEventArgs e)
+        {
+			mainwindow().WindowState = WindowState.Minimized;
         }
 
-        private void minimbtn_Click(object sender, RoutedEventArgs e)
-        {
 
-        }
-    }
+	}
 }
