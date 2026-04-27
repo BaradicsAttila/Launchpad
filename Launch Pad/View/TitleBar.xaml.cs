8505e4c62fd6a380d0c2f6cfa42eeeee09afaa6e
BaradicsAttila <baradics.attila@gmail.com>
2026-04-27 19:16:47 +0200

    Finally fixed the titlebar

--- Launch Pad/View/TitleBar.xaml.cs
+++ Launch Pad/View/TitleBar.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -9,42 +11,79 @@ using CommunityToolkit.Mvvm.Input;
 namespace Launch_Pad.View
 {
     public partial class TitleBar : UserControl
-	{
-		public TitleBar()
-		{
-			InitializeComponent();
-		}
-		//private Window mainwindow(){
-		//	return Window.GetWindow(this);
-		//}
-		
-		//private void clsbtn_Click(object sender, RoutedEventArgs e)
-		//{
-		//	Environment.Exit(0);
-		//}
+    {
+        public TitleBar()
+        {
+            InitializeComponent();
+        }
 
+        private Point mouseDownPos;
+        private bool leftmousebtndonw = false;
 
-		//private void maximbtn_Click(object sender, RoutedEventArgs e)
-  //      {
-		//	if (mainwindow().WindowState == WindowState.Normal)
-		//	{
-		//		maximbtn.Content = "";
-		//		mainwindow().WindowState = WindowState.Maximized;
+        [DllImport("user32.dll")]
+        private static extern bool ReleaseCapture();
 
-		//	}
-		//	else
-		//	{
-		//		maximbtn.Content = "";
-		//		mainwindow().WindowState = WindowState.Normal;
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
 
-		//	}
-		//}
+        private const int WM_NCLBUTTONDOWN = 0x00A1;
+        private const int HTCAPTION = 0x02;
 
-		//private void minimbtn_Click(object sender, RoutedEventArgs e)
-  //      {
-		//	mainwindow().WindowState = WindowState.Minimized;
-  //      }
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount > 1) return;
 
+            var window = Window.GetWindow(this);
+            if (window == null) return;
 
-	}
+            if (window.WindowState == WindowState.Maximized)
+            {
+                mouseDownPos = e.GetPosition(window);
+                leftmousebtndonw = true;
+                Mouse.Capture(this, CaptureMode.SubTree);
+            }
+            else
+            {
+                window.DragMove();
+            }
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            var window = Window.GetWindow(this);
+            if (window == null) return;
+
+            if (!leftmousebtndonw || window.WindowState != WindowState.Maximized) return;
+
+            leftmousebtndonw = false;
+            if (Mouse.Captured == this) Mouse.Capture(null);
+
+            ReleaseCapture();
+            var hwnd = new WindowInteropHelper(window).Handle;
+            SendMessage(hwnd, WM_NCLBUTTONDOWN, new IntPtr(HTCAPTION), IntPtr.Zero);
+
+        }
+
+        private void Window_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            leftmousebtndonw = false;
+            if (Mouse.Captured == this) Mouse.Capture(null);
+        }
+
+        private void TitleBar_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var window = Window.GetWindow(this);
+            if (window == null) return;
+
+            // Toggle maximize / restore on double-click
+            if (window.WindowState == WindowState.Normal)
+            {
+                window.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                window.WindowState = WindowState.Normal;
+            }
+        }
+    }
 }
