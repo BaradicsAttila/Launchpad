e88e8b3db043ea1e6e82ca454c9cac93ac92c04d
baradics.attila <baradics.attila@gmail.com>
2026-05-12 11:33:13 +0200

    progi

--- LaunchPad/Model/Game.cs
+++ LaunchPad/Model/Game.cs
@@ -1,21 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text.Json;
 using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace LaunchPad.Model
 {
     class Game
     {
-        public string Name { get; private set; }
-        public string IconSource { get; private set; }
-        public bool IsFavourite {  get; private set; }
-        public int PlaytimeMinutes { get; private set; }
-        public int LastPlayedHours { get; private set; }
-        public int LaunchCount { get; private set; }
-        public bool IsGame {  get; private set; }
-        public string Source { get; private set; }
-        public bool IsDeleted { get; private set; }
+        public string Name { get; init; }
+        public string IconSource { get; init; }
+        public bool IsFavourite {  get; init; }
+        public int PlaytimeHours { get; init; }
+        public int LastPlayedHours { get; init; }
+        public int LaunchCount { get; init; }
+        public bool IsGame {  get; init; }
+        public string Source { get; init; }
+        public bool IsDeleted { get; init; }
 
+        [JsonConstructor]
+        public Game(
+            string name, 
+            string iconSource, 
+            bool isFavourite,
+            int playtimeHours, 
+            int lastPlayedHours, 
+            int launchCount,
+            bool isGame, 
+            string source, 
+            bool isDeleted)
+        {
+            Name = name;
+            IconSource = iconSource;
+            IsFavourite = isFavourite;
+            PlaytimeHours = playtimeHours;
+            LastPlayedHours = lastPlayedHours;
+            LaunchCount = launchCount;
+            IsGame = isGame;
+            Source = source;
+            IsDeleted = isDeleted;
+        }
     }
 }
